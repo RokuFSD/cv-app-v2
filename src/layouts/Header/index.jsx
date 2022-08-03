7d2236e2be9236657c38f1f759d37bad5d89d0c8
RokuFSD <emilianordx@gmail.com>
2022-08-02 21:56:25 -0300

    Fixed width footer & header

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header
-      className="container w-100 flex items-center h-24 bg-gray-800 text-neutral-200 px-6 text-2xl
+      className="w-full flex items-center h-24 bg-gray-800 text-neutral-200 px-6 text-2xl
     absolute top-0"
     >
       CV-App
