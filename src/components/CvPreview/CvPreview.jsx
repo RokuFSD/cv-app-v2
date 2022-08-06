4bb039873689166d200ea927c6554dec907c785b
RokuFSD <emilianordx@gmail.com>
2022-08-05 22:46:39 -0300

    Started CVPreview

---
+++ src/components/CvPreview/CvPreview.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CvPreview() {
+  return (
+    <section>
+      <div>description</div>
+      <div>experience</div>
+    </section>
+  );
+}
+
+export default CvPreview;
--- src/layouts/Main/index.jsx
+++ src/layouts/Main/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormContainer from "../../components/FormContainer/FormContainer";
+import CvPreview from "../../components/CvPreview/CvPreview";
 import { FormProvider } from "../../context/FormContext";
 
 function Main() {
@@ -7,6 +8,7 @@ function Main() {
     <main className="bg-gray-800 pt-24 pb-16 px-4 h-full sm:px-6">
       <FormProvider>
         <FormContainer />
+        <CvPreview />
       </FormProvider>
     </main>
   );
