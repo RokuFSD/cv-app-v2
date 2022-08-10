fc2575bc0abd076660a4ade3cf8f16741f00e47b
RokuFSD <emilianordx@gmail.com>
2022-08-10 18:05:33 -0300

    Fixed css on header

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Header() {
   return (
     <header
-      className="w-full flex items-center h-24 bg-gray-800 text-neutral-200 px-24 text-3xl
-    absolute top-0"
+      className="w-full flex items-center h-24 bg-gray-800 text-neutral-200 justify-center text-3xl
+     absolute top-0 lg:justify-start px-24"
     >
       CV GENERATOR
     </header>
