556387679120f5917f1bb83ce5de67bf88ae8ea4
RokuFSD <emilianordx@gmail.com>
2022-08-09 22:06:52 -0300

    Fixed photo css

--- src/components/CvPreview/CvPreviewPersonal.jsx
+++ src/components/CvPreview/CvPreviewPersonal.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function CvPreviewPersonal({ items }) {
   return (
     <aside className="w-4/12 min-h-full bg-neutral-300">
-      <img src={items.photo} alt="profile" />
+      <img
+        src={items.photo ? items.photo : "../../../public/PngItem_5798505.png"}
+        alt="profile"
+        className="h-52 max-h-56 object-cover"
+      />
       <div className="flex flex-col mx-2 break-words flex">
         <h2 className="text-blue-400 font-semibold">Personal Details</h2>
         <h2 className="font-semibold text-sm">Address</h2>
