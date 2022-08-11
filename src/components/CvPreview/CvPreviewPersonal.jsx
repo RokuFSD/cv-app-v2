eb56906fdd523a86e4c039435f2d644d79f1086c
RokuFSD <emilianordx@gmail.com>
2022-08-11 00:02:15 -0300

    Fixed default photo route

--- src/components/CvPreview/CvPreviewPersonal.jsx
+++ src/components/CvPreview/CvPreviewPersonal.jsx
@@ -4,7 +4,7 @@ function CvPreviewPersonal({ items }) {
   return (
     <aside className="w-4/12 min-h-full bg-neutral-300">
       <img
-        src={items.photo ? items.photo : '../../../public/PngItem_5798505.png'}
+        src={items.photo ? items.photo : '/PngItem_5798505.png'}
         alt="profile"
         className="h-52 max-h-56 object-cover"
       />
